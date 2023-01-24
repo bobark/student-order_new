0b18fcc8f39e97eb04336a0b167dcf7f12d9c68c
bobark <bobark@yandex.ru>
2023-01-24 13:16:30 +0700

    First JUnit Test

---
+++ src/test/java/edu/javacourse/studentorder/dao/DictionaryDaoImpTest.java
@@ -0,0 +1,11 @@
+package edu.javacourse.studentorder.dao;
+
+import org.junit.Test;
+
+public class DictionaryDaoImpTest {
+
+    @Test
+    public void testExample() {
+        System.out.println("TEST");
+    }
+}
