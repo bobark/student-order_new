96e163a8c0a83d619eb892ae993076ef6ef34293
bobark <bobark@yandex.ru>
2023-01-25 16:24:04 +0700

    Lesson 69;  Unit tests for Dictionary

--- src/test/java/edu/javacourse/studentorder/dao/DictionaryDaoImpTest.java
+++ src/test/java/edu/javacourse/studentorder/dao/DictionaryDaoImpTest.java
@@ -1,9 +1,14 @@
 package edu.javacourse.studentorder.dao;
 
 
+import edu.javacourse.studentorder.domain.CountryArea;
+import edu.javacourse.studentorder.domain.PassportOffice;
+import edu.javacourse.studentorder.domain.RegisterOffice;
+import edu.javacourse.studentorder.domain.Street;
+import edu.javacourse.studentorder.exception.DaoException;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 
 import java.net.URL;
 import java.nio.file.Files;
@@ -38,20 +43,34 @@ public class DictionaryDaoImpTest {
 
 
     @Test
-    public void testExample1() {
-        System.out.println("TEST 1");
+    public void testStreet() throws DaoException {
+        List<Street> d = new DictionaryDaoImp().findStreet("про");
+        Assert.assertTrue(d.size() == 2);
     }
 
     @Test
-//    @Ignore
-    public void testExample2() {
-        System.out.println("TEST 2");
+    public void testPassportOffice() throws DaoException {
+        List<PassportOffice> po = new DictionaryDaoImp().findPassportOffice("010020000000");
+        Assert.assertTrue(po.size() == 2);
     }
 
     @Test
-    public void testExample3() {
-        System.out.println("TEST 3");
-//        throw new RuntimeException("Bad result");
+    public void testRegisterOffice() throws DaoException {
+        List<RegisterOffice> ro = new DictionaryDaoImp().findRegisterOffice("010010000000");
+        Assert.assertTrue(ro.size() == 2);
     }
+
+    @Test
+    public void testArea() throws DaoException {
+        List<CountryArea> co1 = new DictionaryDaoImp().findArea("");
+        Assert.assertTrue(co1.size() == 2);
+        List<CountryArea> co2 = new DictionaryDaoImp().findArea("020000000000");
+        Assert.assertTrue(co2.size() == 2);
+        List<CountryArea> co3 = new DictionaryDaoImp().findArea("020010000000");
+        Assert.assertTrue(co3.size() == 2);
+        List<CountryArea> co4 = new DictionaryDaoImp().findArea("020010010000");
+        Assert.assertTrue(co4.size() == 2);
+    }
+
 
 }
--- src/test/java/edu/javacourse/studentorder/dao/SimpleRunner.java
+++ src/test/java/edu/javacourse/studentorder/dao/SimpleRunner.java
@@ -1,7 +1,6 @@
 package edu.javacourse.studentorder.dao;
 
 import org.junit.Test;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
